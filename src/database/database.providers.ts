eb91b23ad3e20cd41c890ee95ad0b3d5dc56c9f2
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-07-20 11:06:54 -0300

    feat: Add database configuration

---
+++ src/database/database.providers.ts
@@ -0,0 +1,10 @@
+import * as mongoose from 'mongoose';
+import configurationConfig from 'src/config/configuration.config';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (): Promise<typeof mongoose> =>
+      await mongoose.connect(configurationConfig().connectionString),
+  },
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import configurationConfig from './config/configuration.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(configurationConfig().port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
