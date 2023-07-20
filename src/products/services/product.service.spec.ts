03191b5aeb8955e90abc1ac4d3b94100f98e55c5
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-07-20 17:05:17 -0300

    test: Add tests for service

--- src/products/services/product.service.spec.ts
+++ src/products/services/product.service.spec.ts
@@ -4,13 +4,39 @@ import { productProvider } from '../providers/product.provider';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../../database/database.module';
 import configuration from '../../config/configuration.config';
+import { getModelToken } from '@nestjs/mongoose';
+import { ProductDto } from '../dto/product.dto';
+import { Model } from 'mongoose';
+import { Product } from '../models/product.interface';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let model: any;
+
+  const mockedProduct = {
+    name: 'Product 1',
+    description: 'Description',
+    price: 4,
+    createdAt: new Date(10, 1, 2022),
+  };
+
+  const token = 'PRODUCT_MODEL';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService, ...productProvider],
+      providers: [
+        ProductService,
+        ...productProvider,
+        {
+          provide: token,
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockedProduct),
+            constructor: jest.fn().mockResolvedValue(mockedProduct),
+            find: jest.fn(),
+            create: jest.fn().mockReturnValue(mockedProduct),
+          },
+        },
+      ],
       imports: [
         ConfigModule.forRoot({
           load: [configuration],
@@ -21,9 +47,31 @@ describe('ProductService', () => {
     }).compile();
 
     service = module.get<ProductService>(ProductService);
+    model = module.get<Model<Product>>(token);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('#create', () => {
+    it('should create a product', async () => {
+      // ARRANGE
+      const createProduct: ProductDto = {
+        description: 'Product 1',
+        name: 'Description',
+        price: 4,
+      };
+
+      jest
+        .spyOn(model, 'create')
+        .mockImplementationOnce(() => Promise.resolve(mockedProduct));
+
+      // ACT
+      const result = await service.create(createProduct);
+
+      // ASSERT
+      expect(result).toBe(mockedProduct);
+    });
   });
 });
