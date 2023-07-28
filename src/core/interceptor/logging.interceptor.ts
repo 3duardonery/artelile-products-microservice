7f4b5689f291b3578909c26503493e98b5adabeb
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-07-28 14:15:36 -0300

    feat: Configuring logging

--- src/core/interceptor/logging.interceptor.ts
+++ src/core/interceptor/logging.interceptor.ts
@@ -9,12 +9,20 @@ import { Observable, tap } from 'rxjs';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('Before...');
-    console.log(context.getArgs());
+    console.log('Executing...');
+    const request = context.switchToHttp().getRequest();
+    const url = request.url;
+    const headers = request.headers;
+    const body = request.body;
+    console.log({
+      url: url,
+      headers: headers,
+      body: body,
+    });
 
     const now = Date.now();
     return next
       .handle()
-      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+      .pipe(tap(() => console.log(`Done... ${Date.now() - now}ms`)));
   }
 }
