761e20dc0e65028b2aea9e6527cba425fa3c9dff
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-07-28 14:03:58 -0300

    feat: Add logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
+import { CoreModule } from './core/core.module';
 import configuration from './config/configuration.config';
 
 @Module({
@@ -14,6 +15,7 @@ import configuration from './config/configuration.config';
     }),
     DatabaseModule,
     ProductsModule,
+    CoreModule,
   ],
   controllers: [AppController],
   providers: [AppService],
