e2115ffd83db4b4cb3f92c11034a4070bee6b1ea
Eduardo Nery <edu.nery.cordeiro@gmail.com>
2023-08-17 15:58:11 -0300

    feat: Add vercel config

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configurationConfig from './config/configuration.config';
 import { ValidationPipe } from '@nestjs/common';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
